b9f937393e4fbbbb0bcf7407020b86b8a1e93a0c
najilav <najila.vilakkini@gmail.com>
2016-09-26 15:31:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,11 +6,46 @@ var app = express();
 app.use(morgan('combined'));
 
 var content={
-    title:"",date:"",
+    title:"Article One",date:"21/09/2016",heading:"Article One",
     content:`hi this is article one.
              A day will come when the people of Pakistan will go against their own government to fight terrorism. India has been successful in isolating you (Pakistan) globally, and we will intensify our efforts to make sure that you are isolated globally. The people of Pakistan should ask their leaders that India and Pakistan got freedom in the same year, but India exports software and your (Pakistan) leaders are exporting terrorists," said Prime Minister Modi, while addressing a public rally in Kozhikode`
 };
-
+function createTemplate(data)
+{
+  var title=data.title;
+  var heading=data.heading;
+  var date=data.date;
+  var content=data.content;
+  
+  var htmlTemplate = 
+   ` <html>
+    <head>
+        <title> ${title}</title>
+        <meta name="viewport" content="width:device-width,initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <div>
+                 <h1>${heading}</h1>
+            </div>
+            <div>
+                date:${date}
+            </div>
+            <div>
+                <p>
+                    ${content}
+                </p>
+            </div>
+        </div>
+    </body>
+    </html>`;
+    return htmlTemplate;
+}
+    
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
