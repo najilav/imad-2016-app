de1d21144059952566408e3239bc7221c0a84ac4
najilav <najila.vilakkini@gmail.com>
2016-09-26 15:57:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ app.use(morgan('combined'));
 
 
 var articles={ 
-    articleOne:{title:"Article One",date:"21/09/2016",heading:"Article One",
+    'article-one':{title:"Article One",date:"21/09/2016",heading:"Article One",
                 content:`hi this is article one.
                          A day will come when the people of Pakistan will go against their own government to fight terrorism. India has been successful in isolating you (Pakistan) globally, and we will intensify our efforts to make sure that you are isolated globally. The people of Pakistan should ask their leaders that India and Pakistan got freedom in the same year, but India exports software and your (Pakistan) leaders are exporting terrorists," said Prime Minister Modi, while addressing a public rally in Kozhikode`},
-    articleTwo:{title:"Article Two",date:"22/09/2016",heading:"Article Two",
+    'article-two':{title:"Article Two",date:"22/09/2016",heading:"Article Two",
                 content:`hi this is article two.
                          A day will come when the people of Pakistan will go against their own government to fight terrorism. India has been successful in isolating you (Pakistan) globally, and we will intensify our efforts to make sure that you are isolated globally. The people of Pakistan should ask their leaders that India and Pakistan got freedom in the same year, but India exports software and your (Pakistan) leaders are exporting terrorists," said Prime Minister Modi, while addressing a public rally in Kozhikode`},
-    articleThree:{title:"Article Three",date:"21/09/2016",heading:"Article Three",
+    'article-three':{title:"Article Three",date:"21/09/2016",heading:"Article Three",
                 content:`hi this is article three.
                          A day will come when the people of Pakistan will go against their own government to fight terrorism. India has been successful in isolating you (Pakistan) globally, and we will intensify our efforts to make sure that you are isolated globally. The people of Pakistan should ask their leaders that India and Pakistan got freedom in the same year, but India exports software and your (Pakistan) leaders are exporting terrorists," said Prime Minister Modi, while addressing a public rally in Kozhikode`}
     
@@ -59,8 +59,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.send(createTemplate(articleOne));
+app.get('/:articleName', function (req, res) {
+//   var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
