2ee3a0f6831ffecbad9465f41d8908fae845d6cd
najilav <najila.vilakkini@gmail.com>
2016-09-26 17:34:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,15 @@
 console.log('Loaded!');
 var element=document.getElementById('main-txt');
 element.innerHTML="Najila Vilakkini";
+
+var img=document.getElementById('dino');
+img.onclick= function(){
+    var interval = setInterval(moveRight,100);
+}
+
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft = marginLeft+10;
+    img.css.marginLeft = marginLeft+'px';
+}
